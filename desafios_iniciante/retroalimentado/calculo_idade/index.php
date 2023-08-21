34f0eb26c9a039c7e10b43c93b06d340eba70506
Suskz <ielsondias1@gmail.com>
2023-08-21 17:28:57 -0300

    Calculo_idade

--- desafios_iniciante/retroalimentado/calculo_idade/index.php
+++ desafios_iniciante/retroalimentado/calculo_idade/index.php
@@ -11,12 +11,24 @@
         <h1>Calculando sua idade</h1>
         <?php 
             $actyear = date("Y");
+            $age = $_GET["age"] ?? 0;
+            $year = $_GET["year"] ?? 0;
         ?>
         <form action="" method="get">
             <label for="age">Em que ano você nasceu?</label>
             <input type="number" name="age" id="idage">
             <label for="year">Quer saber sua idade em que ano?(Atualmente estamos em <strong><?= $actyear?></strong>)</label>
+            <input type="number" name="year" id="idyear" min="<?= $age + 1?>" value="<?= $year?>" >
+            <input type="submit" value="Calcular idade">
         </form>
     </main>
+
+    <section>
+        <h2>Resultado</h2>
+        <?php 
+            $test = $year - $age;
+            echo "Quem nasceu em $age vai ter <strong>$test anos</strong> em $year!"
+        ?>
+    </section>
 </body>
 </html>
